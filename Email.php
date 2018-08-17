068dce8869d608af182ad1613a66a0f4ba0c112c
Gilberto Vargas <ticox.vargas@gmail.com>
2018-08-17 15:54:20 -0500

    configurado email con bluehost.com

--- Email.php
+++ Email.php
@@ -5,18 +5,17 @@ include "lib/email/class.smtp.php";
 $email_user = "info@cotedem.com";
 $email_password = "Cotedem@2018";
 $the_subject = "Contacto Por Pagina web";
-$from_name = "Sender";
-$attach1 = "prueba de envio de email";
+$from_name = "Prueba de envio de email por la pagina web";
+$attach1 = "Mensaje: Este es un email de prueba";
 $phpmailer = new PHPMailer();
 
-// ---------- datos de la cuenta de correo -----------------------------
+// ---------- Datos de la cuenta de correo -----------------------------
 $phpmailer->Username = $email_user;
 $phpmailer->Password = $email_password; 
 //---------------------------------------------------------------------
 $phpmailer->SMTPSecure = 'ssl';
 $phpmailer->Host = "box308.bluehost.com";
 $phpmailer->Port = 465;
-//$mail->Port = 465; $mail->SMTPSecure = 'ssl';
 //$phpmailer->SMTPDebug = 2;
 $phpmailer->IsSMTP();
 $phpmailer->SMTPAuth = true;
@@ -25,7 +24,7 @@ $phpmailer->setFrom($phpmailer->Username,$from_name);
 $phpmailer->AddAddress("soporte@cotedem.com");
 $phpmailer->Subject ="Prueba"; 
 
-$phpmailer->Body .="<h1 style='color:#3498db;'>Attachment:</h1>";
+$phpmailer->Body .="<h1 style='color:#3498db;'>".$from_name.":</h1>";
 $phpmailer->Body .= "<h3>".$attach1."</h3>";
 
 $phpmailer->AddAttachment($attach1, "attach1");
