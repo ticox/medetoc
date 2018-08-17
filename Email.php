f647dae5fb15278f0e58591e10d43fdb661b9c61
Gilberto Vargas <ticox.vargas@gmail.com>
2018-08-17 14:54:55 -0500

    class phpmailer agregada

--- Email.php
+++ Email.php
@@ -1,33 +1,35 @@
 <?php
-include "phpmailer/class.phpmailer.php";
-include "phpmailer/class.smtp.php";
+include "lib/email/class.phpmailer.php";
+include "lib/email/class.smtp.php";
 
-$email_user = "email@host.com";
-$email_password = "pass123";
-$the_subject = "Title";
+$email_user = "info@cotedem.com";
+$email_password = "Cotedem@2018";
+$the_subject = "Contacto Por Pagina web";
 $from_name = "Sender";
+$attach1 = "prueba de envio de email";
 $phpmailer = new PHPMailer();
 
 // ---------- datos de la cuenta de correo -----------------------------
 $phpmailer->Username = $email_user;
 $phpmailer->Password = $email_password; 
 //---------------------------------------------------------------------
-$phpmailer->SMTPSecure = 'tls';
-$phpmailer->Host = "box6171.bluehost.com";
-$phpmailer->Port = 26;
+$phpmailer->SMTPSecure = 'ssl';
+$phpmailer->Host = "box308.bluehost.com";
+$phpmailer->Port = 465;
+//$mail->Port = 465; $mail->SMTPSecure = 'ssl';
 //$phpmailer->SMTPDebug = 2;
 $phpmailer->IsSMTP();
 $phpmailer->SMTPAuth = true;
 
 $phpmailer->setFrom($phpmailer->Username,$from_name);
-$phpmailer->AddAddress("to@host.com");
-$phpmailer->Subject = $the_subject; 
+$phpmailer->AddAddress("soporte@cotedem.com");
+$phpmailer->Subject ="Prueba"; 
 
 $phpmailer->Body .="<h1 style='color:#3498db;'>Attachment:</h1>";
 $phpmailer->Body .= "<h3>".$attach1."</h3>";
 
-$phpmailer->AddAttachment($attach, "attach1");
-$phpmailer->AddBCC("hidecopy@host.com", "bcc1");
+$phpmailer->AddAttachment($attach1, "attach1");
+$phpmailer->AddBCC("soporte@cotedem.com", "bcc1");
 $phpmailer->IsHTML(true);
 $enviado = $phpmailer->Send();
 if($enviado) {
